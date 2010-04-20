cfa32eab56eef6935116770c11ce011acc58a9e3
Nathan Esquenazi <nesquena@gmail.com>
2010-04-20 03:46:48 -0700

    Added root route and about route to app file

--- app/app.rb
+++ app/app.rb
@@ -3,4 +3,12 @@ class MongoBlog < Padrino::Application
     register SassInitializer
     enable :sessions
   end
+
+  get :root, :map => "/" do
+    redirect url(:posts, :index)
+  end
+
+  get :about do
+    render :haml, "%p Demonstrates the power of Padrino and MongoDB!"
+  end
 end
