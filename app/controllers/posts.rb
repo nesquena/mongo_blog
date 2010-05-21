1b4bd2871cc5dbedd487f3aaf38acf66703458b3
Arthur Chiu <Mr.Arthur.Chiu@gmail.com>
2010-05-21 01:23:34 -0700

    posts index show

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -1,5 +1,12 @@
 MongoBlog.controllers :posts do
+  
   get :index do
     render 'posts/index'
   end
+  
+  get :show, :with => :permalink do
+    @post = Post.where(:permalink => params[:permalink]).first
+    render 'posts/show'
+  end
+  
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,13 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/../test_config.rb')
 
-class PostsControllerTest < Test::Unit::TestCase
-  context "PostsController" do
-    setup do
-      get '/'
-    end
+context "PostsController" do
+  setup { Post.delete_all }
+  
+  context "get index" do    
+    setup { get("/posts") }
 
-    should "return hello world text" do
-      assert_equal "Hello World", last_response.body
+    asserts("Hello World") { last_response.body }.matches %r{Hello World}
+  end
+  
+  context "get show" do
+    setup do
+      post = Post.create(:title => "Hello World",:body => "mongo blog is here")
+      get "/posts/show/#{post.permalink}"
     end
+    asserts("shows post body") { last_response.body }.matches %r{mongo blog is here}
+    asserts("shows post title") { last_response.body }.matches %r{Hello World}
   end
 end
